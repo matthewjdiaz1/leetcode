470e3f3ef9d731aed5de266d077252ee3d512bab
Matthew-Jacob-Diaz <matthewjdiaz1@gmail.com>
2020-01-10 10:14:35 -0800

    solution found

--- leetcode/784.js
+++ leetcode/784.js
@@ -2,14 +2,25 @@
 // https://leetcode.com/problems/letter-case-permutation/submissions/
 
 var letterCasePermutation = function (S) {
-  // create output array
-  // split S into chars
+  let output = [];
+  let chars = S.split('');
 
-  // recursively traverse chars, (i, str)
-  //// if no more chars, push str to output and return out of recursion
-  //// else if chars[i] is a #, concat string and traverse next index
-  //// else, create uppercase and lowercase version and traverse next index
+  // recursively traverse chars
+  const traverseChars = (i, str) => {
+    if (i === chars.length) {
+      // if no more chars, push str to output and return out of recursion
+      output.push(str);
+      return;
+    } else if (!isNaN(chars[i])) {
+      // else if chars[i] is a #, concat string and traverse next index
+      traverseChars(i + 1, str += chars[i]);
+    } else {
+      // else, create uppercase and lowercase version and traverse next index
+      traverseChars(i + 1, str + chars[i].toUpperCase());
+      traverseChars(i + 1, str + chars[i].toLowerCase());
+    }
+  };
 
-  // start recursion
-  // return output;
+  traverseChars(0, '');
+  return output;
 };
