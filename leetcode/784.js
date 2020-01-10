ce7bcc263b4a40b568667e1969854fa3e14162c3
Matthew-Jacob-Diaz <matthewjdiaz1@gmail.com>
2020-01-10 10:10:31 -0800

    pseudocoded solution

---
+++ leetcode/784.js
@@ -0,0 +1,15 @@
+// 784. Letter Case Permutation
+// https://leetcode.com/problems/letter-case-permutation/submissions/
+
+var letterCasePermutation = function (S) {
+  // create output array
+  // split S into chars
+
+  // recursively traverse chars, (i, str)
+  //// if no more chars, push str to output and return out of recursion
+  //// else if chars[i] is a #, concat string and traverse next index
+  //// else, create uppercase and lowercase version and traverse next index
+
+  // start recursion
+  // return output;
+};
