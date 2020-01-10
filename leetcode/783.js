79e672233c837ef4f5c63c3e9173ecab4618597c
Matthew-Jacob-Diaz <matthewjdiaz1@gmail.com>
2020-01-10 12:20:32 -0800

    nonoptimal solution

--- leetcode/783.js
+++ leetcode/783.js
@@ -1,15 +1,30 @@
+// 783. Minimum Distance Between BST Nodes
+// https://leetcode.com/problems/minimum-distance-between-bst-nodes/
+
+
 var minDiffInBST = function (root) {
   let output = Infinity;
 
   // traverse tree function and compare every value with passed in value
   const traverseTree = (node, val) => {
     // if val doesn't exists
-    //// recurse left and/or right with & w/o current node val ( this will try all possibilities of tree, unnoptimal as tree is sorted)
-
-    // else
-    //// compare current node with passed in val
-    //// if left or right exists, recurse with val
-
+    if (!val) {
+      // recurse left and/or right with & w/o current node val
+      if (node.left) {
+        traverseTree(node.left);
+        traverseTree(node.left, node.val);
+      }
+      if (node.right) {
+        traverseTree(node.right);
+        traverseTree(node.right, node.val);
+      }
+    } else {
+      // compare current node with passed in val
+      if (Math.abs(node.val - val) < output) { output = Math.abs(node.val - val); }
+      // if left or right exists, recurse with val
+      if (node.left) { traverseTree(node.left, val); }
+      if (node.right) { traverseTree(node.right, val); }
+    }
   }
   traverseTree(root);
   // return output
