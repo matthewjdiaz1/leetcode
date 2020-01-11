0e1582ceb11c175c1a02c7bc3ac1aac370a0f6dd
Matthew-Jacob-Diaz <matthewjdiaz1@gmail.com>
2020-01-10 23:54:40 -0800

    5% ms, 100% memory solution

--- leetcode/970.js
+++ leetcode/970.js
@@ -3,14 +3,20 @@
 
 var powerfulIntegers = function (x, y, bound) {
   let output = [];
+  // find largest x^n and y^n < bound, 
+  // suboptimal approach, must find optimal x and y max (5%)
+  let largestPow = Math.sqrt(bound);
 
-  // find largest x^n and y^n < bound, (maybe + 1?)
-
-  // for loop i and jfor x and y while n < largestPow
-
-  // if (x^i + y^j) < bound push into output (no duplicates)
-
-  // output.push(powerInt);
-
+  // for loop for x and y while n <= largestPow
+  for (let i = 0; i <= largestPow; i++) {
+    for (let j = 0; j <= largestPow; j++) {
+      let powerInt = Math.pow(x, i) + Math.pow(y, j);
+      // if (x^i + y^j) < bound push into output (no duplicates)
+      if (powerInt <= bound && !output.includes(powerInt)) {
+        // push
+        output.push(powerInt);
+      }
+    }
+  }
   return output;
 };
