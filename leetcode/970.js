2c23f08e30c0ece2d4abb5340e692f72a2856cf1
Matthew-Jacob-Diaz <matthewjdiaz1@gmail.com>
2020-01-10 23:50:56 -0800

    psuedocoded suboptimal solution

---
+++ leetcode/970.js
@@ -0,0 +1,16 @@
+// 970. Powerful Integers
+// https://leetcode.com/problems/powerful-integers/
+
+var powerfulIntegers = function (x, y, bound) {
+  let output = [];
+
+  // find largest x^n and y^n < bound, (maybe + 1?)
+
+  // for loop i and jfor x and y while n < largestPow
+
+  // if (x^i + y^j) < bound push into output (no duplicates)
+
+  // output.push(powerInt);
+
+  return output;
+};
