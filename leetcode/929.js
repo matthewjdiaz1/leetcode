c391f554aa98b9b45d9599b4eb869c0128a3f75e
Matthew-Jacob-Diaz <matthewjdiaz1@gmail.com>
2020-01-22 15:32:34 -0800

    pseudocode

---
+++ leetcode/929.js
@@ -0,0 +1,22 @@
+// 929. Unique Email Addresses
+// https://leetcode.com/problems/unique-email-addresses/
+
+
+const numUniqueEmails = function (emails) {
+  let output = [];
+
+  // for each email
+
+  // split by '@'
+
+  // remove all '.' from local name
+
+  // remove anything after and including '+' if exists
+
+  // reattatch local name to domain name
+
+  // compare to storage
+
+  // return output.length;
+  return output.length;
+};
